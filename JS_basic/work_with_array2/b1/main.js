fb1f350335d5107d680fb7de61e6462bf45eb15f
Long <88829179+longdp121@users.noreply.github.com>
2022-05-21 15:40:06 +0700

    map() practice

--- JS_basic/work_with_array2/b1/main.js
+++ JS_basic/work_with_array2/b1/main.js
@@ -37,37 +37,39 @@ var courses = [
   },
 ];
 
-// // forEach()
-// courses.forEach(function(course, index) {
-//     console.log(index, course)
-// })
+// forEach()
+courses.forEach(function (course, index) {
+  // console.log(index, course)
+  return index, course;
+});
 
-// //every()
-// courses.every(function(course) {
-//     console.log(course.coint === 0)
-// })
+//every()
+courses.every(function (course) {
+  // console.log(course.coint === 0)
+  return course.coint === 0;
+});
 
-// //some()
-// courses.some(function(course, index) {
-//     console.log(course.coint === 0)
-//     return course.coint === 0
-// })
+//some()
+courses.some(function (course, index) {
+  // console.log(course.coint === 0)
+  return course.coint === 0;
+});
 
-// //find()
-// console.log(
-//     courses.find(function(course, index) {
-//         return course.name == "JS"
-//     })
-// )
+//find()
+let find = courses.find(function (course, index) {
+  return course.name == "JS";
+});
 
-// //filter
-// console.log(
-//     courses.filter(function(course, index) {
-//         return course.coint === 0
-//     })
-// )
+// console.log(find)
 
-//map()
+//filter
+let filter = courses.filter(function (course, index) {
+  return course.coint != 0;
+});
+
+// console.log(filter)
+
+// map()
 var courseHandler = function (course, index) {
   return {
     id: course.id,
@@ -79,17 +81,37 @@ var courseHandler = function (course, index) {
 
 var newCourses = courses.map(courseHandler);
 
-console.log(newCourses);
+// console.log(newCourses);
 
-//reduce()
+// reduce()
 var i = 0;
 function cointHandler(accumulator, currentValue, currentIndex, originArray) {
-    // i++;
-    // console.log(i, accumulator)
-    // console.log(currentValue)
-    return accumulator + currentValue.coint
+  // i++;
+  // console.log(i, accumulator)
+  // console.log(currentValue)
+  return accumulator + currentValue.coint;
 }
 
 var totalCoin = courses.reduce(cointHandler, 0);
 
-console.log(totalCoin)
+// console.log(totalCoin)
+
+//Practice
+
+let myList = [
+  { name: "abc", age: 100 },
+  { name: "jqk", age: 77 },
+  { name: "cde", age: 1000 },
+];
+
+function tellYearBorn(person) {
+  let year = new Date().getFullYear();
+  return {
+    name: person.name,
+    yearBorn: year - person.age,
+  };
+}
+
+let newList = myList.map(tellYearBorn);
+
+console.log(newList)
